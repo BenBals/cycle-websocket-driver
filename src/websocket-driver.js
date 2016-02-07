06af9769d51ef535cce93a40973d65f12e8456f8
Ben <benjustusbals@gmail.com>
2016-02-07 17:43:24 +0100

    ups. forgot something

---
+++ src/websocket-driver.js
@@ -0,0 +1,24 @@
+import Rx from 'rx';
+
+function makeWebsocketDriver (url) {
+    let ws = new WebSocket(url);
+    return function websocketDriver (requests$) {
+        let $responses = Rx.Observable.create(function (observer) {
+            ws.onmessage = msg => {
+                console.log('received message:');
+                console.log(msg);
+                observer.onNext(msg);
+            };
+            ws.onopen = ev => console.log('connection opened');
+            ws.onerror = ev => console.log(ev);
+        });
+
+        requests$.subscribe(request => {
+            console.log('sending message: ' + request);
+            ws.send(request);
+        });
+        return $responses;
+    };
+};
+
+export default makeWebsocketDriver;
