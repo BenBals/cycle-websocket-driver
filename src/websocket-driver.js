c4ff1635397519401d4981d730fb5b36144bd532
BenBals <benjustusbals@gmail.com>
2016-02-16 19:54:54 +0100

    babel instead of webpack, require instead of ES modules

--- src/websocket-driver.js
+++ src/websocket-driver.js
@@ -1,24 +1,24 @@
-import Rx from 'rx';
+let rx = require ('rx')
 
 function makeWebsocketDriver (url) {
-    let ws = new WebSocket(url);
+    let ws = new WebSocket(url)
     return function websocketDriver (requests$) {
         let $responses = Rx.Observable.create(function (observer) {
             ws.onmessage = msg => {
-                console.log('received message:');
-                console.log(msg);
-                observer.onNext(msg);
-            };
-            ws.onopen = ev => console.log('connection opened');
-            ws.onerror = ev => console.log(ev);
-        });
+                console.log('received message:')
+                console.log(msg)
+                observer.onNext(msg)
+            }
+            ws.onopen = ev => console.log('connection opened')
+            ws.onerror = ev => console.log(ev)
+        })
 
         requests$.subscribe(request => {
-            console.log('sending message: ' + request);
-            ws.send(request);
-        });
-        return $responses;
-    };
-};
+            console.log('sending message: ' + request)
+            ws.send(request)
+        })
+        return $responses
+    }
+}
 
-export default makeWebsocketDriver;
+export default makeWebsocketDriver
